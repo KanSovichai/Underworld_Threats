cc0f20b9aa617e3e9983e161d27e5668c29dc52f
Kan Sovichai <ksovichai08@gmail.com>
2026-07-24 00:26:49 +0700

    Created Dante page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import "../styles/Footer.css";
+
+const Footer = () => {
+	return (
+		<footer>
+			<div className="Footer_container">
+				<div className="left_container"></div>
+				<div className="right_container">
+					<div className="about_us_container">
+						<h2>ABOUT US</h2>
+						<ul>
+							<li>
+								<a>About Us</a>
+							</li>
+							<li>Terms & Conditions</li>
+							<li>EULA</li>
+							<li>Privacy Policy</li>
+						</ul>
+					</div>
+					<div className="contact_us_container">
+						<h2>CONTACT US</h2>
+						<ul>
+							<li>
+								<a>
+									Contact Us
+								</a>
+							</li>
+							<li>Email</li>
+							<li>Phone</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<hr />
+			<div className="copyright">
+                <h3>©2026 copyright all reserve Underworld Threats</h3>
+            </div>
+		</footer>
+	);
+};
+export default Footer;
--- src/pages/HomeDante.jsx
+++ src/pages/HomeDante.jsx
@@ -1,31 +1,81 @@
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import "../styles/HomeDante.css";
 import GothicDante from "../assets/resource/Gothic_not_dante.webp";
+import DanteSword from "../assets/resource/Dante_sword.jpg";
+
 const Home = (props) => {
 	return (
 		<div className="container">
-			<div className="nav_wrapper">
-				<Navbar status={props.status}></Navbar>
-			</div>
-			<div className="hero_text_container">
-				<h1>The devil Hunter</h1>
+			<div className="hero_container">
+				<div className="nav_wrapper">
+					<Navbar status={props.status}></Navbar>
+				</div>
+				<div className="hero_text_container">
+					<h1>The devil Hunter</h1>
+				</div>
 			</div>
 			<div className="description_container">
-				<img src={GothicDante} alt="" />
+				<img src={GothicDante} id="gothic_dante_img" alt="" />
 				<div className="context_container">
-					<div className="text_container">
-						<h1>Dante</h1>
-						<p>
-							Dante is the iconic protagonist of Capcom's Devil May Cry action
-							series—a half-human, half-demon demon hunter who operates out of a
-							gothic, demon-plagued city. As the son of the legendary Dark
-							Knight Sparda (a demon who rebelled against his own kind) and the
-							human Eva, Dante inherited immense supernatural power while
-							retaining his humanity.
-						</p>
+					<div className="dante_bg_wrapper">
+						<div className="text_container">
+							<h1>Dante</h1>
+							<p>
+								Dante is the iconic protagonist of Capcom's Devil May Cry action
+								series—a half-human, half-demon demon hunter who operates out of
+								a gothic, demon-plagued city. As the son of the legendary Dark
+								Knight Sparda (a demon who rebelled against his own kind) and
+								the human Eva, Dante inherited immense supernatural power while
+								retaining his humanity.
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
+
+			<div className="feature_dante_container">
+				<div className="broken_wood_wrapper"></div>
+			</div>
+
+			<div className="banner_wrapper">
+				<div className="dante_sword_wrapper">
+					<img src={DanteSword} alt="" />
+				</div>
+                <h1 className="banner_text">Get the son of sparda's items today.</h1>
+			</div>
+
+            <div className="items_container">
+                <div className="items">
+
+
+                    <div className="item_card">
+                        <div className="frame_style"></div>
+                        <div className="content">
+                            <div className="item_img">
+                                <img src="https://m.media-amazon.com/images/I/71gt1HB3kDL._AC_UY350_.jpg" alt="" />
+                            </div>
+                            <div className="item_title">
+                                <h1>Dante Sword</h1>
+                            </div>
+                            <button className="view_button">
+                                VIEW
+                            </button>
+                        </div>
+                    </div>
+
+                    
+                
+                
+                </div>
+                <div className="dante_pic_right_banner">
+                </div>
+            </div>
+
+            <div className="footer_wrapper">
+                <Footer></Footer>
+            </div>
+
 		</div>
 	);
 };
