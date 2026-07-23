081d160c4ccea7a518a780496ebc6fc0fd854e87
Kan Sovichai <ksovichai08@gmail.com>
2026-07-23 20:21:59 +0700

    Update some of the feature for Dante home page

--- src/pages/HomeDante.jsx
+++ src/pages/HomeDante.jsx
@@ -1,15 +1,32 @@
 import Navbar from "../components/Navbar";
 import "../styles/HomeDante.css";
+import GothicDante from "../assets/resource/Gothic_not_dante.webp";
 const Home = (props) => {
-    return(
-        <div className="container">
-            <div className="nav_wrapper">
-                <Navbar status={props.status}></Navbar>
-            </div>
-            <div className="hero_text_container">
-                <h1>The devil Hunter</h1>
-            </div>
-        </div>
-    );
-}
+	return (
+		<div className="container">
+			<div className="nav_wrapper">
+				<Navbar status={props.status}></Navbar>
+			</div>
+			<div className="hero_text_container">
+				<h1>The devil Hunter</h1>
+			</div>
+			<div className="description_container">
+				<img src={GothicDante} alt="" />
+				<div className="context_container">
+					<div className="text_container">
+						<h1>Dante</h1>
+						<p>
+							Dante is the iconic protagonist of Capcom's Devil May Cry action
+							series—a half-human, half-demon demon hunter who operates out of a
+							gothic, demon-plagued city. As the son of the legendary Dark
+							Knight Sparda (a demon who rebelled against his own kind) and the
+							human Eva, Dante inherited immense supernatural power while
+							retaining his humanity.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 export default Home;
