04444f0ca41e25c1b5fc314362c7119220232d08
Kan Sovichai <ksovichai08@gmail.com>
2026-07-26 01:01:24 +0700

    Constructing vergil homepage

--- src/pages/HomeVergil.jsx
+++ src/pages/HomeVergil.jsx
@@ -1,15 +1,29 @@
 import Navbar from "../components/Navbar";
 import "../styles/HomeVergil.css";
 const HomeVergil = (props) => {
-    return(
-        <div className="vergil_container">
-            <div className="navbar_wrapper">
-				<Navbar status={props.status} ></Navbar>
+	return (
+		<div>
+            <div className="hero_vergil_container">
+                <div className="vergil_navbar_wrapper">
+					<Navbar status={props.status}></Navbar>
+				</div>
+				<div className="hero_vergil_contents">
+					<div className="slash_effect">
+							
+					</div>
+					<div className="hero_vergil_text">
+						<h1>The Alpha & Omega</h1>
+					</div>
+					<div className="hero_vergil_img">
+						
+					</div>
+				</div>
             </div>
-            
-        </div>
-    );
-}
-
+			<div className="vergil_container">
+				
+			</div>
+		</div>
+	);
+};
 
 export default HomeVergil;
